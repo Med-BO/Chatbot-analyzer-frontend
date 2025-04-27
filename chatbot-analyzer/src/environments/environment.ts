7b239158df0f0e077d78cae6170a4c006eba589f
Mohamed Ben Othman <Mohamedbenothman2@outlook.com>
2025-04-27 14:02:12 +0100

    Added excel export functionality

--- chatbot-analyzer/src/environments/environment.ts
+++ chatbot-analyzer/src/environments/environment.ts
@@ -4,8 +4,8 @@
 
 export const environment = {
   production: false,
-  // apiUrl: 'https://chatbot-analyzer-backend.onrender.com/api'
-  apiUrl: 'http://localhost:5000/api',
+  apiUrl: 'https://chatbot-analyzer-backend.onrender.com/api'
+  // apiUrl: 'http://localhost:5000/api',
 };
 
 /*
